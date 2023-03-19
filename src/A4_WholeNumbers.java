874f3e1f8a081e7d196b2ee8691c416e84b8b716
badmood111 <petrzela.adam@seznam.cz>
2023-03-19 09:34:13 +0100

    2.4 added gitignore

--- src/A4_WholeNumbers.java
+++ src/A4_WholeNumbers.java
@@ -1,2 +1,10 @@
 public class A4_WholeNumbers {
+    public static void main(String[] args) {
+        //int globalPopulation = 8_000_000_000; error
+        long globalPopulation = 8_000_000_000L;
+        System.out.println("The global population is: " + globalPopulation);
+
+        long dailyGoogleSearches = 8_500_000_000L;
+        System.out.println("Daily, there are " + dailyGoogleSearches + " Google searches.");
+    }
 }
---
+++ src/A5_Decimals.java
@@ -0,0 +1,10 @@
+public class A5_Decimals {
+    public static void main(String[] args) {
+        double percentage = 73.5;
+        System.out.println(percentage + "% are made up.");
+
+        double wallet = 20;
+        int people = 3;
+        System.out.println(wallet / people);
+    }
+}
